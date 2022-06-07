941a2715b7d191398aba6fbcfbb1a20f9300b5ca
luoshuai <luoshuai@inke.cn>
2022-06-07 20:19:50 +0800

    2022-06-07 20:19:50

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-now=$(date "+%Y-%m-%d")
-echo "Starting add-commit-pull-push..."
+now=$(date "+%Y-%m-%d %H:%M:%S")
+echo "starting add-commit-pull-push..."
 git add -A && git commit -m "$now" && git pull && git push
 echo "Finish!"
